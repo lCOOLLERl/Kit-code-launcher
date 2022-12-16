b45daf7276ae9bcfe34191955c13b636f626fd74
cooller1995 <cooller1995@mail.ru>
2022-12-16 18:36:33 +0700

    7

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ const { Microsoft } = require('minecraft-java-core');
 const { autoUpdater } = require('electron-updater')
 
 const path = require('path');
+
 const fs = require('fs');
 
 const UpdateWindow = require("./assets/js/windows/updateWindow.js");
